2c8d3376129bac0e710c254fb93f37a4dec41001
k <chen_kui_jun@s2023.ssts.edu.sg>
2026-01-30 14:31:54 +0800

    testing excel and rename home to main

---
+++ pages/4_Practices.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+from openpyxl import Workbook
+
+# wb = Workbook()
+# ws = wb.active
+# ws.title = "Practice"
+# wb.save("Testing.xlsx")
+
+# df = pd.read_excel("Testing.xlsx")
+# st.dataframe(df)
+
+
+# st.data_editor()
